f8e15227366f3efc07f2633a7cb765e6d102e744
pohlman24 <reece.pohlman@gmail.com>
2021-02-10 20:42:01 -0500

    set up screen and pieces

---
+++ main.py
@@ -0,0 +1,53 @@
+import pygame
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First Game")
+
+WHITE = (255, 255, 255)
+FPS = 60
+VEL = 5
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load('D:\Python\pygame_intro\Assets\spaceship_yellow.png')
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load('D:\Python\pygame_intro\Assets\spaceship_red.png')
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    
+def draw_window(red, yellow):
+    WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x,yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
+    pygame.display.update()
+
+
+def main():
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_a]:  # LEFT
+            yellow.x -= VEL
+        elif keys_pressed[pygame.K_d]:  # RIGHT
+            yellow.x += VEL
+        elif keys_pressed[pygame.K_w]:  # UP
+            yellow.y -= VEL
+        elif keys_pressed[pygame.K_s]:  # DOWN
+            yellow.y += VEL
+
+        draw_window(red, yellow)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
